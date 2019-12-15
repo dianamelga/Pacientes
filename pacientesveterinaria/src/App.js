7dcb027eca4f8070ff9ff661a80fcd6aad9e118c
dianamelga <di.melgarejo@gmail.com>
2019-12-15 01:58:17 -0300

    hasta 54, creacion de cita y listado

---
+++ pacientesveterinaria/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./bootstrap.min.css";
+import Header from "./components/Header";
+import NuevaCita from "./components/NuevaCita";
+import ListaCitas from "./components/ListaCitas";
+
+class App extends Component {
+  state = {
+    citas:[]
+  };
+
+  crearNuevaCita = datos => {
+    //copiar state actual
+    const citas = [...this.state.citas, datos];
+
+    //agregar el nuevo state
+    this.setState({
+      citas : citas
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Header titulo="Administrador Pacientes Veterinaria"></Header>
+
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NuevaCita crearNuevaCita={this.crearNuevaCita} />
+          </div>
+
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas
+              citas={this.state.citas}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ pacientesveterinaria/src/components/NuevaCita.js
@@ -0,0 +1,166 @@
+import React, { Component } from "react";
+import uuid from 'uuid';
+
+const stateInicial = {
+    error: false,
+    cita: {
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: ""
+    }
+}
+
+class NuevaCita extends Component {
+  state = { ...stateInicial };
+
+  handleChange = e => {
+    console.log(e.target.name + ": " + e.target.value);
+
+    //colocar lo que el usuario escribe en el state
+    this.setState({
+      cita: {
+        ...this.state.cita, //copia del state para no perder valores
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  //cuando el usuario envia el formulario
+  handleSubmit = e => {
+    e.preventDefault();
+
+    //extraer valores del state
+    const { mascota, propietario, fecha, hora, sintomas } = this.state.cita;
+
+    //validar que todos los camos esten llenos
+    if (
+      mascota === "" ||
+      propietario === "" ||
+      fecha === "" ||
+      hora === "" ||
+      sintomas === ""
+    ) {
+      this.setState({
+        error: true
+      });
+
+      return;
+    }
+
+    //generar objeto con los datos
+    const nuevaCita = {...this.state.cita};
+    nuevaCita.id = uuid();
+
+    //agregar la cita al state de App
+    this.props.crearNuevaCita(nuevaCita);
+
+    //colocar en el state el stateInicial
+    this.setState({
+        ...stateInicial
+    })
+  };
+
+  render() {
+    //extraer valor del state
+    const {error} = this.state;
+
+    return (
+      <div className="card mt-5 py-5">
+        <div className="card-body">
+          <h2 className="card-title text-center mb-5">
+            Llena el formulario para crear una nueva cita
+          </h2>
+
+           
+          {//alerta en caso de error
+          error ? <div className="alert alert-danger mt-2 mb-5 
+          text-center">Todos los campos son obligatorios</div> : null}
+
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-2 col-form-label">
+                Nombre Mascota
+              </label>
+              <div className="col-sm-8 col-lg-10">
+                <input
+                  id="mascota"
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Mascota"
+                  name="mascota"
+                  onChange={this.handleChange}
+                  value={this.state.cita.mascota}
+                />
+              </div>
+            </div>{" "}
+            {/* cierre form group */}
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-2 col-form-label">
+                Nombre Dueño
+              </label>
+              <div className="col-sm-8 col-lg-10">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Dueño"
+                  name="propietario"
+                  onChange={this.handleChange}
+                  value={this.state.cita.propietario}
+                />
+              </div>
+            </div>{" "}
+            {/* cierre form group */}
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
+              <div className="col-sm-8 col-lg-4">
+                <input
+                  type="date"
+                  className="form-control"
+                  name="fecha"
+                  onChange={this.handleChange}
+                  value={this.state.cita.fecha}
+                />
+              </div>
+
+              <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
+              <div className="col-sm-8 col-lg-4">
+                <input
+                  type="time"
+                  className="form-control"
+                  name="hora"
+                  onChange={this.handleChange}
+                  value={this.state.cita.hora}
+                />
+              </div>
+            </div>{" "}
+            {/* cierre form group */}
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-2 col-form-label">
+                Síntomas
+              </label>
+              <div className="col-sm-8 col-lg-10">
+                <textarea
+                  className="form-control"
+                  placeholder="Describe los Síntomas"
+                  name="sintomas"
+                  onChange={this.handleChange}
+                  value={this.state.cita.sintomas}
+                ></textarea>
+              </div>
+            </div>{" "}
+            {/* cierre form group */}
+            <input
+              type="submit"
+              className="py-3 mt-2 btn btn-success btn-block"
+              value="Agregar Nueva Cita"
+            ></input>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NuevaCita;
